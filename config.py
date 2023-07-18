d520adf08e6d23e201b3d286508281a95ca0ba04
MikhailZhi <zhirovmal@gmail.com>
2023-07-18 23:47:00 +0500

    Added connection to an existing PostgreSQL DB

--- config.py
+++ config.py
@@ -2,3 +2,9 @@
 # @Pasr_Sb_bot:
 
 tg_token = '5908912730:AAHTNy2hlbkjhjFRDr_Fp3m6gy8bBWX6hzE'
+drive_file_path = r'D:\Users\Михаил\gdrive_off\2023_Project_A\Project_A.xlsx'
+
+host = "127.0.0.1"
+user = "postgres"
+password = "Schedule20"
+db_name = "tg_bot"
